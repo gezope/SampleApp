30e876476bf377857f0e64e9bb4dc07b6cd0b6ef
Zoltan Gero <gezope@gmail.com>
2011-06-07 01:15:59 +0200

    added test for routes and then match to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Sampleapp::Application.routes.draw do
 
-  get "users/new"
+  match '/signup',  :to => 'users#new'
 
   match '/contact', :to => "pages#contact"
   match '/about', :to => "pages#about"
   match '/help', :to => "pages#help"
   match '/home', :to => "pages#home"
+
   root :to => "pages#home"
 
 #  get "pages/home"
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -22,4 +22,10 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Help")
   end
 
+  it "should have a Signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+
+
 end
