3736c0733faf927228b7eb5644b2c94084461cf7
Zoltan Gero <gezope@gmail.com>
2011-06-06 23:04:33 +0200

    removed index and chenged routes, all tests fails

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Sampleapp::Application.routes.draw do
 
-  get "pages/home"
+  match '/contact', :to => "pages#contact"
+  match '/about', :to => "pages#about"
+  match '/help', :to => "pages#help"
+  root :to => "pages#home"
 
-  get "pages/about"
-
-  get "pages/contact"
-
-  get "pages/help"
+#  get "pages/home"
+#  get "pages/about"
+#  get "pages/contact"
+#  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
