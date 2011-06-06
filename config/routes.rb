2a3fefef0bac44f18d03cd3930e9b72f49d49e2a
Zoltan Gero <gezope@gmail.com>
2011-06-06 23:13:49 +0200

    routes fixed and footer and header added with _path s

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Sampleapp::Application.routes.draw do
   match '/contact', :to => "pages#contact"
   match '/about', :to => "pages#about"
   match '/help', :to => "pages#help"
+  match '/home', :to => "pages#home"
   root :to => "pages#home"
 
 #  get "pages/home"
