e737e441bbc4fb1ebf20fae81ed20a46ea5c1ba6
Zoltan Gero <gezope@gmail.com>
2011-06-08 13:09:15 +0200

    user model syntax fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,6 @@ class User < ActiveRecord::Base
 
   validates :name, :presence => true,
                    :length => { :maximum => 50 }
-  validates :email, :presence => true
+  validates :email, :presence => true,
                     :format   => { :with => email_regex }
 end
