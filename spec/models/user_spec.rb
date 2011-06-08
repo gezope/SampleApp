db48b7371b322933e9f2556b481a34a181bb28b7
Zoltan Gero <gezope@gmail.com>
2011-06-08 03:03:37 +0200

    user longname spec and validations added

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,15 @@ describe User do
     no_name_user = User.new(@attr.merge( :name => "" ))
     no_name_user.should_not be_valid
   end
+  
+  it "should require an email address" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+  
+  it "should reject names which are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
 end
